dff3af2a7ffc9bf39d7335fcd2b8d725828dcfd4
zwc <you@example.com>
2024-05-03 10:38:55 -0400

    Add script by cyj

---
+++ sync_comment.sh
@@ -0,0 +1,6 @@
+# 将 peer 的注释和 vp0 同步
+cid=`docker ps | grep debug-vp0 | awk '{print $1}'`
+dst="/go/src/github.com/hyperledger/fabric/peer"
+docker cp peer $cid:$dst
+
+zzm=`peer chaincode deploy -p github.com/hyperledger/fabric/examples/chaincode/go/chaincode_example02 -c '{"Function":"init", "Args": ["a","100", "b", "200"]}'` | awk '{print $3}'
