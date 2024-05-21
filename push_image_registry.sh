f96b6f567bd9e5cc90554eaaca2440383f081676
zwc <you@example.com>
2024-05-21 22:29:45 +0800

    feat: 增加上传镜像脚本

---
+++ push_image_registry.sh
@@ -0,0 +1,2 @@
+docker tag peer-image registry.cn-beijing.aliyuncs.com/tanyongfeng/fabirc-0.6-peer-image:latest
+docker push registry.cn-beijing.aliyuncs.com/tanyongfeng/fabirc-0.6-peer-image:latest
